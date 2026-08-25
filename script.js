ae3d3eacfa9850551015f5969f8a7da858f6eb74
Joyfulhub3272 <173010792+Joyfulhub3272@users.noreply.github.com>
2026-08-25 05:12:10 +0100

    made the dot interractive, and added a structure for the hamburger and sidebar for mobile view

---
+++ script.js
@@ -0,0 +1,54 @@
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+const nextButton = document.querySelector("#next");
+const prevButton = document.querySelector("#prev");
+let currentSlide = 0;
+
+// Function for showing a slide
+function showSlide(index) {
+  if (index < 0 || index >= slides.length) {
+    return;
+  }
+
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+
+  dots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+
+  slides[index].classList.add("active");
+  dots[index].classList.add("active");
+
+  currentSlide = index;
+}
+
+// Next button listener
+nextButton.addEventListener("click", () => {
+  let nextslide = currentSlide + 1;
+
+  if (nextslide >= slides.length) {
+    nextslide = 0;
+  }
+  showSlide(nextslide);
+});
+
+// Previous Button slide
+prevButton.addEventListener("click", () => {
+  let previousslide = currentSlide - 1;
+
+  if (previousslide < 0) {
+    previousslide = slides.length - 1;
+  }
+
+  showSlide(previousslide);
+});
+
+// To make the dot active
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    showSlide(index);
+  });
+});
