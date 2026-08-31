9203f5b3d50d0f36237a6dbfc964b57f01f6c376
Joyfulhub3272 <173010792+Joyfulhub3272@users.noreply.github.com>
2026-08-31 22:49:59 +0100

    Added watsapp mesaage feature

--- js/contact.js
+++ js/contact.js
@@ -11,3 +11,37 @@ menuLinks.forEach((link) => {
     mobileMenu.classList.remove("show");
   });
 });
+
+// Addded Whatsapp function
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Get the user informations from the field
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
+
+  const whatsappMessage =
+    "Hello PrimeNest, %0A%0A" +
+    "My name is " +
+    name +
+    ".%0A" +
+    "Email: " +
+    email +
+    "%0A" +
+    "Subject: " +
+    subject +
+    "%0A" +
+    "Message: " +
+    message;
+
+  const whatsappNumber = "2349157968653";
+
+  const whatsappURL =
+    "https://wa.me/" + whatsappNumber + "?text=" + whatsappMessage;
+
+  window.open(whatsappURL, "_blank");
+});
